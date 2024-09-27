e2bfcc0531566c52e8cfd4ea48debc1be3e4d7c7
shiina <152995083+shiina4119@users.noreply.github.com>
2024-09-27 14:55:20 +0530

    Add LoadBalancer struct

--- internal/loadbalancer.go
+++ internal/loadbalancer.go
@@ -1,8 +1,48 @@
 package internal
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
-type loadBalancer interface {
+type balancer interface {
 	ServeHTTP(http.ResponseWriter, *http.Request)
 	HealthCheck()
+}
+
+type LoadBalancer struct {
+	pool balancer
+	algo string
+}
+
+func NewLoadBalancer(s []*Server, a string) *LoadBalancer {
+	var p balancer
+	switch a {
+	case "round-robin":
+		p = NewRRPool(s)
+	}
+	return &LoadBalancer{
+		pool: p,
+		algo: a,
+	}
+}
+
+func (lb *LoadBalancer) Serve(addr string) {
+	t := time.NewTicker(time.Second * 2)
+	go func() {
+		defer t.Stop()
+		for {
+			<-t.C
+			log.Println("Health check underway")
+			lb.pool.HealthCheck()
+		}
+	}()
+
+	s := &http.Server{
+		Addr:    addr,
+		Handler: lb.pool,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
